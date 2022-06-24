457f2791059de776184dc58995c530ede7a0f841
Teerapon Meesuk <98877466+teerapon1996@users.noreply.github.com>
2022-06-24 10:43:40 +0700

    Delete Landing directory

--- Landing/backend/api/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-from .v1.routers import router as router_v1
-urlpatterns = [
-    path('v1/', include(router_v1.urls))
-]
--- Landing/backend/runserver.sh
+++
@@ -1,8 +0,0 @@
-while ! nc -w 1 -z ${PROJECT_NAME}_db 5432;
-do sleep 5;
-done;
-
-python manage.py makemigrations
-python manage.py makemigrations landing
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
